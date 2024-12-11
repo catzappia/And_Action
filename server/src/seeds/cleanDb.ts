578497137a4d19c00452d3e7f7e25a9b96dc302e
Catherine Zappia <catzappia@aol.com>
2024-12-11 15:10:07 -0500

    Update cleanDb.ts with original code

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,15 +3,9 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    if (!models[modelName] || !models[modelName].db) {
-      throw new Error(`Model or database not found for modelName: ${modelName}`);
-    }
-    let modelExists = [];
-    if (models[modelName].db.db) {
-      modelExists = await models[modelName].db.db.listCollections({
-        name: collectionName
-      }).toArray();
-    }
+    let modelExists = await models[modelName].db.db.listCollections({
+      name: collectionName
+    }).toArray()
 
     if (modelExists.length) {
       await db.dropCollection(collectionName);
